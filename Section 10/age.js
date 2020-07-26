538d64de07d900a02408cdcc5a2c9bdb26b610c0
komal7292 <kg681278@gmail.com>
2020-07-26 19:21:41 +0530

    Delete

--- Section 10/age.js
+++
@@ -1,4 +0,0 @@
-var age = prompt("what is your age?");
-var days = age * 365;
-alert(age +" "+ "years age is roughly " + " " + days + " " + "days" + ".");
-alert("Thanks for using me.");
--- Section 10/komal.js
+++
@@ -1,5 +0,0 @@
-var firstName = prompt("What is your dog name?");
-var lastName = prompt("What is your last name?");
-var age = prompt("What is your age?");
-alert("Your full name is " + firstName + " " + lastName +  ".");
-alert("Your age is " + age + ".");
