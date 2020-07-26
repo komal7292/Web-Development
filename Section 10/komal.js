a991f245ef949df941650a86bb21e92167936b76
komal7292 <kg681278@gmail.com>
2020-07-26 19:15:48 +0530

    Section 10

---
+++ Section 10/komal.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your dog name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+alert("Your full name is " + firstName + " " + lastName +  ".");
+alert("Your age is " + age + ".");
