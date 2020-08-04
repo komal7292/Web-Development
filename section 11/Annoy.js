cd6ab7c5a8fd96eca3282430595997754b7ef4ce
komal7292 <kg681278@gmail.com>
2020-08-04 15:42:39 +0530

    add

---
+++ section 11/Annoy.js
@@ -0,0 +1,17 @@
+//var answer = prompt("Are you from India?");
+
+//while (answer !== "yes") {
+//    var answer = prompt("Are you from India?");    
+}
+//alert("Yes, I am from India.");
+
+
+
+//version 2
+
+var answer = prompt("Are you from India?");
+
+while (answer.indexOf("yes") === -1) {
+ var answer = prompt("Are you from India?");    
+}
+alert("Yes, I am from India.");
---
+++ section 11/For.js
@@ -0,0 +1,5 @@
+document.write("PRINTING ALL NUMBER BETWEEN -10 AND 19");
+
+for(var i = -10; i < 20; i++) {
+    document.write(i);
+}
