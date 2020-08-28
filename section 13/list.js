98b2e45811a806924d3203e5adf7b869e1060130
komal7292 <kg681278@gmail.com>
2020-08-28 15:07:28 +0530

    pull

---
+++ section 13/list.js
@@ -0,0 +1,15 @@
+window.setTimeout(function () {
+  var todos = ['BUY NEW PEN']
+
+  var input = window.prompt('what would you like to do?')
+  while (input !== 'quit') {
+    if (input === 'list') {
+      console.log(todos)
+    } else if (input === 'new') {
+      var newTodo = window.prompt('Enter New Todo')
+      todos.push(newTodo)
+    }
+    input = window.prompt('what would you like to do?')
+  }
+  console.log('OK, YOU QUIT THE APP')
+}, 500)
