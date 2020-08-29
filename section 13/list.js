b0ce7fac65952a1fb2d05e6d6710bad39a882090
komal7292 <kg681278@gmail.com>
2020-08-29 19:55:48 +0530

    commit

--- section 13/list.js
+++ section 13/list.js
@@ -1,14 +1,34 @@
 window.setTimeout(function () {
   var todos = ['BUY NEW PEN']
+
   var input = window.prompt('what would you like to do?')
   while (input !== 'quit') {
     if (input === 'list') {
-      console.log(todos)
+      listTodos()
     } else if (input === 'new') {
-      var newTodo = window.prompt('Enter New Todo')
-      todos.push(newTodo)
+      addTodos()
+    } else if (input === 'delete') {
+      deleteTodo()
     }
     input = window.prompt('what would you like to do?')
   }
   console.log('OK, YOU QUIT THE APP')
+
+  function listTodos () {
+    console.log('---------')
+    todos.forEach(function (todo, i) {
+      console.log(i + ': ' + todo)
+    })
+    console.log('---------')
+  }
+  function addTodos () {
+    var newTodo = window.prompt('Enter New Todo')
+    todos.push(newTodo)
+    console.log('Added Todo')
+  }
+  function deleteTodo () {
+    var index = window.prompt('Enter index of todo to delete')
+    todos.splice(index, 1)
+    console.log('Deleted Todo')
+  }
 }, 500)
