0368535efcd9656473296021d9e9f13b816cf412
komal <kg681278@gmail.com>
2020-09-14 17:40:15 +0530

    renamed folder

--- Section-12/solution.js
+++ Section-12/solution.js
@@ -1,23 +1,23 @@
-//function isEven (num) {
-  //if (num % 3 === 0) {
-    //return true
-  //} else {
-    //return false
-  //}
-//}
+// function isEven (num) {
+// if (num % 3 === 0) {
+// return true
+// } else {
+// return false
+// }
+// }
 
-//function isEven(num){
-  //return num % 2 === 0;
-//}
-//function factorial(num) {
-  //var result = 1;
-  //for(var i = 2; i <= num; i++){
-    //result *= i;
-  //}
-  //return result;
-//}
+// function isEven(num){
+// return num % 2 === 0;
+// }
+// function factorial(num) {
+// var result = 1;
+// for(var i = 2; i <= num; i++){
+// result *= i;
+// }
+// return result;
+// }
 
-function kebabToSnake(str) {
-  var newStr = str.replace(/-/g , "_");
+function kebabToSnake (str) {
+  var newStr = str.replace(g, '_')
   return newStr
 }
