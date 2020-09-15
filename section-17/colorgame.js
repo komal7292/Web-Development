3c5df52546828f47842dfac0621c06177864938a
komal <kg681278@gmail.com>
2020-09-15 15:12:17 +0530

    added some new codes

--- section-17/colorgame.js
+++ section-17/colorgame.js
@@ -7,8 +7,9 @@ var colors = [
   'rgb(255, 0, 255)'
 ]
 var squares = document.querySelectorAll('.square')
-var pickedColor = colors[4]
+var pickedColor = pickColor()
 var colorDisplay = document.getElementById('colorDisplay')
+var messageDisplay = document.querySelector('#message')
 
 colorDisplay.textContent = pickedColor
 
@@ -21,9 +22,22 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.background
     // complare colors to picked colors
     if (clickedColor === pickedColor) {
-      window.alert('correct!!')
+      messageDisplay.textContent = 'Correct!!'
+      changeColors(clickedColor)
     } else {
-      window.alert('wrong!!!')
+      this.style.background = '#232323'
+      messageDisplay.textContent = 'Try Again'
     }
   })
+}
+
+function changeColors (color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = color
+  }
+}
+
+function pickColor () {
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random]
 }
