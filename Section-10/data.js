4548417896ef3c448e010e772b44176c1e488c66
komal <kg681278@gmail.com>
2020-09-19 16:59:01 +0530

    added some files

---
+++ Section-10/data.js
@@ -0,0 +1,29 @@
+// Primitives data types
+
+// Strings
+
+var name = 'komal'
+console.log('My name is ' + name + '.')
+console.log('Data type of ' + (typeof name) + '.')
+
+// Numbers
+
+var marks = 17
+console.log('Data type of ' + (typeof marks) + '.')
+
+// boolean
+
+var isDriver = true
+console.log('Data type of ' + (typeof isDriver) + '.')
+
+// null
+
+var nullVar = null
+console.log('Data type of ' + (typeof nullVar) + '.')
+
+// Reference data types
+
+// Arrays
+var myArr = [1, 2, 3, 4]
+console.log(myArr)
+console.log('Data type of ' + (typeof myArr) + '.')
